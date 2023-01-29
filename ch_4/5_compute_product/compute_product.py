29ec34d22daaf4a8b93a273f83147981c23a4646
John Davies <jd.other@hotmail.com>
2023-01-29 17:40:36 +0000

    compute product question written

---
+++ ch_4/5_compute_product/compute_product.py
@@ -0,0 +1,12 @@
+"""
+Write a program that multiplies two nonnegative integers. The only operators you are allowed to use are:
+- assignment
+- the bitwise operators >>, <<, |, &, ~, ^
+- equality checks and boolean combinations thereof
+
+You may use loops and functions that you write yourself. These constraints imply, for example,
+that you cannot use increment or decrement, or test if x < y.
+
+Hint: Add using bitwise operations, multiply using shift-and-add
+"""
+
