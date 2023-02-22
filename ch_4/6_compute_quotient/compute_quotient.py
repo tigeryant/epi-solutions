ac72ce3bad2570b1b40dd01e56109682ad5409f6
John Davies <jd.other@hotmail.com>
2023-02-22 16:06:07 +0000

    brute force compute quotient solution

--- ch_4/6_compute_quotient/compute_quotient.py
+++ ch_4/6_compute_quotient/compute_quotient.py
@@ -4,3 +4,25 @@ Compute quotient without arithmetical operators
 Given two positive integers, compute their quotient, using only the addition, subtraction and shifting operators
 '''
 
+# brute force
+
+def divide_brute_force(x, y):
+    # iteratively subtract y from x
+    # each iteration, we increment the quotient
+    # each iteration we also check the result
+
+    remainder = x - y
+    if remainder < 0:
+        return 0
+    if remainder == 0:
+        return 1
+
+    quotient = 0
+    while remainder >= 0:
+        remainder = remainder - y
+        quotient += 1
+    return quotient
+
+x = 24
+y = 6
+print(f'The quotient derived from dividing {x} by {y} is {divide_brute_force(x, y)}')
