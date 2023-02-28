ccd73f73696057f08c2ca21f55e1bd0e3c147821
John Davies <jd.other@hotmail.com>
2023-02-28 15:21:39 +0000

    integer palindrome setup

---
+++ ch_4/9_integer_palindrome/integer_palindrome.py
@@ -0,0 +1,9 @@
+"""
+A palindromic string is one which reads the same forwards and backwards, e.g., "redivider". In this problem, you are to write a program which
+determines if the decimal representation of an integer is a palindromic string. For example, your program should return true for the inputs
+0, 1, 7, 11, 121, 333, and 2147447412, and false for the inputs -1, 12, 100, and 2147483647.
+
+Write a program that takes an integer and determines if that integer's representation as a decimal string is a palindrome.
+"""
+
+
