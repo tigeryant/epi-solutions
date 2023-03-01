7491c1fd88db287decca351d6bcbbec22ea138bc
John Davies <jd.other@hotmail.com>
2023-03-01 16:34:16 +0000

    implemented random number generator initial solution

--- ch_4/10_uniform_random_numbers/uniform_random_numbers.py
+++ ch_4/10_uniform_random_numbers/uniform_random_numbers.py
@@ -10,7 +10,7 @@ import random
 
 # random number generator that produces zero or one with equal probability
 def zero_one_random():
-    return random.randint(0, 1)
+    return True if random.randint(0, 1) else False
 
 # if x is a power of 2, we could iteratively flip the coin x/2 times. Each time, we split the space in half
 # and return the half that is determined by the coin toss (lower or upper half depending on 0 or 1)
@@ -18,3 +18,33 @@ def zero_one_random():
 
 # How do we deal with the case in which x is not a power of 2?
 
+# We could have a series of rounds of coin tossing
+# Each round, we iterate over the remaining 'players'
+# In each iteration, we assign that player either a 0 or a 1 based on the coin toss
+# If a player's coin toss returns a 0, they are eliminated.
+# The player (integer) who gets the most 1's in a row (i.e is the only one left after all others have been eliminated) is selected/returned
+
+# Really, we don't need a tally of how many 1's each player has, we just need to know who's still in the game and eventually
+# return the one who hasn't been eliminated yet
+
+# Suppose we have a list of booleans of length x where x is the upper bound, or the number of players. Each element is set to True
+
+def rng(x):
+    players = []
+    for _ in range(x):
+        players.append(True)
+    # add a while loop that continues until a 'winner' is selected(only 1 element of the players list is equal to true)
+    still_true = x
+    while still_true > 1:
+        for i in range(len(players)):
+            if still_true == 1:
+                break
+            if players[i] is True:
+                players[i] = zero_one_random()
+                if players[i] is False:
+                    still_true -= 1
+
+    return players.index(True) + 1 # index of player that's true
+
+x = 5
+print(f'players: {rng(x)}')
